bc213a9fcfd535503c20e8b2de754b7cdb26981a
Dade1991 <dade91@msn.com>
2025-06-16 16:52:35 +0200

    Added other details to HTML

--- js/chart.js
+++ js/chart.js
@@ -17,17 +17,3 @@ new Chart(chart, {
     ],
   },
 })
-
-// const chart = document.getElementById("myChart")
-// const myChart = new Chart(chart, {
-//   type: "doughnut",
-//   data: {
-//     labels: ["correct", "wrong"],
-//     datasets: [
-//       {
-//         label: "Risposte",
-//         data: [75, 25],
-//       },
-//     ],
-//   },
-// })
