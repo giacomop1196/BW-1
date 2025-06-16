78140ca41d29588f69d8805c3c026d1b12e22116
Francesco Dicecca <147667176+Francesco-3@users.noreply.github.com>
2025-06-17 00:06:56 +0200

    v1.0.0.1

--- js/script.js
+++ js/script.js
@@ -120,31 +120,45 @@
 
       // BUON LAVORO 💪🚀
 
-const stelle = [];
-const span = document.getElementById("stella");
-for(let i = 1; i <=10; i++){
-    const img = document.createElement('img');
-    img.src = '../assets/images/star.copy.svg';
-    img.dataset.index = i;
-    img.classList.add('stella');
-    img.style.margin = '10px';
+/* ---------- FEEDBACK PAGE ---------- */
+// DIV STAR
+const stars = [];
+const span = document.getElementById("stars");
 
-    img.addEventListener('click',() => {
-      const index = parseInt(img.dataset.index);
-        for(let j = 0; j < stelle.length; j++){
-         if(j <= index) {
-            stelle[j].src = '../assets/images/star.svg';
-         }
-         else{
-            stelle[j].src = '../assets/images/star.copy.svg';
-         }
-        }
-    });
-    stelle.push(img);
-    span.appendChild(img);
+for(let i = 1; i <=10; i++){
+  const img = document.createElement('img');
+  img.src = '../assets/images/star.copy.svg';
+  img.dataset.index = i;
+  img.classList.add('star');
+  img.style.margin = '10px';
+
+  img.addEventListener('click',() => {
+    const index = parseInt(img.dataset.index) -1;
+    for(let j = 0; j < stars.length; j++){
+      if(j <= index) {
+        stars[j].src = '../assets/images/star.svg';
+      } else {
+        stars[j].src = '../assets/images/star.copy.svg';
+      }
+    }
+  });
+  
+  stars.push(img);
+  span.appendChild(img);
 }
+
+// DIV COMMENT
 const button = document.getElementsByClassName('button-style')[0];
+const feedback = document.querySelector('.feedback');
+
 button.addEventListener('click', () => {
   alert('Feedback Inviato');
   location.reload();
 });
+
+feedback.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    button.click();
+  }
+});
+/* ========== FEEDBACK PAGE ========== */
