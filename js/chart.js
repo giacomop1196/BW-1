b8eb7deaf72aecbd1df3d44087a827ccfd85c14d
Dade1991 <dade91@msn.com>
2025-06-16 21:04:19 +0200

    Finshed HTLM structure & CSS additional portions

--- js/chart.js
+++ js/chart.js
@@ -1,6 +1,6 @@
 const chartData = {
-  labels: [`Correct`, `Wrong`],
-  data: [50, 50],
+  labels: [`Wrong`, `Correct`],
+  data: [30, 70],
 }
 
 const chart = document.getElementById(`mainChart`)
@@ -17,13 +17,32 @@ new Chart(chart, {
     ],
   },
   options: {
-    borderWidth: 2,
-    borderRadius: 10,
+    borderWidth: 0,
+    borderRadius: 2,
+    cutout: `70%`,
     hoverBorderWidth: 2,
+    backgroundColor: [` #d20094`, `#00ffff`],
     plugins: {
       legend: {
         display: false,
       },
     },
   },
+  plugins: [
+    {
+      id: "centerText",
+      afterDraw: function (chart) {
+        const ctx = chart.ctx
+        // ctx.save()
+        ctx.font = "24px Arial"
+        ctx.textAlign = "center"
+        ctx.textBaseline = "middle"
+        ctx.fillStyle = "white"
+        const centerX = chart.canvas.width / 1.5
+        const centerY = chart.canvas.height / 1.5
+        ctx.fillText(``, centerX, centerY)
+        // ctx.restore()
+      },
+    },
+  ],
 })
