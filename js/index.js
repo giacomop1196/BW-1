9cf947e3c4c996d46767532314543a7bacc25e0f
giacomop1196 <giacomo.pillitteri65@gmail.com>
2025-06-17 18:42:49 +0200

    commit error fix index

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
+let error = document.querySelector('.error-index')
+
 function startQuiz() {
   const checkBox = document.getElementById("myCheckbox")
   if (checkBox.checked) {
     window.location.href = "./question.html"
   } else {
-    alert("Kindly confirm the below checkbox in order to proceed")
+    error.innerText = "You must first confirm the checkbox"
   }
 }
