c55d67a659f15370ddaf16b106d7de147b2b7a08
Dade1991 <dade91@msn.com>
2025-06-18 16:45:03 +0200

    Other minor changes and final touches

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
-let error = document.querySelector('.error-index')
+let error = document.querySelector(".error-index")
 
 function startQuiz() {
   const checkBox = document.getElementById("myCheckbox")
   if (checkBox.checked) {
     window.location.href = "./question.html"
-    error.style.display = 'none';
+    error.style.display = "none"
   } else {
-    error.style.display = 'block';
-    error.innerText = "You must first confirm the checkbox"
+    error.style.display = "block"
+    error.innerHTML = `<i class="fas fa-exclamation-triangle"></i> You must first confirm the checkbox`
   }
 }
